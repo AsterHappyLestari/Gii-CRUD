477109515c0d9d88b54acdc5751551b9b1710b48
LAPTOP-UC2OHDJ9\USER <asterlestari03@gmail.com>
2021-06-08 15:59:56 +0700

    tugas

--- controllers/MahasiswaController.php
+++ controllers/MahasiswaController.php
@@ -1,64 +1,127 @@
-<?php  
+<?php
+
 namespace app\controllers;
+
 use Yii;
-use yii\web\Controller;
-use yii\data\Pagination;
 use app\models\Mahasiswa;
+use app\models\MasiswaSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
 
 /**
- * 
+ * MahasiswaController implements the CRUD actions for Mahasiswa model.
  */
 class MahasiswaController extends Controller
 {
-	
-	public function actionIndex()
-	{
-		# code...
-		$query = Mahasiswa::find();
-		$pagination = new Pagination(['defaultPageSize' => 5,
-			'totalCount' => $query->count()]);
-		$data_mahasiswa = $query->orderBy('id')
-		->offset($pagination->offset)
-		->limit($pagination->limit)
-		->all();
-		return $this->render('index',
-			['data_mahasiswa' => $data_mahasiswa,
-			'pagination' => $pagination,]);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
 
-	public function actionCreate()
-	{
-		# code...
-		$model = new Mahasiswa();
+    /**
+     * Lists all Mahasiswa models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new MasiswaSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
-		if ($model->load(Yii::$app->request->post()) && $model->save()) {
-			# code...
-			Yii::$app->session->setFlash('success','Sucsess !! Data Sudah Tersimpan ');
-			return $this->refresh();
-		}
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
 
-		return $this->render('create', ['model' => $model,]);
-	}
+    /**
+     * Displays a single Mahasiswa model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
 
-	public function actionUpdate($id)
-	{
-		# code...
-		$model = Mahasiswa::findOne($id);
+    /**
+     * Creates a new Mahasiswa model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Mahasiswa();
 
-		if ($model->load(Yii::$app->request->post()) && $model->save()) {
-			# code...
-			Yii::$app->session->setFlash('success','Success Full!!!');
-			return $this->refresh();
-		}
-		return $this->render('update',['model' => $model,]);
-	}
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
 
-	public function actionDelete($id)
-	{
-		# code...
-		$model = Mahasiswa::findOne($id);
-		$model->delete();
-		return $this->redirect(['index']);
-	}
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing Mahasiswa model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing Mahasiswa model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Mahasiswa model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Mahasiswa the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Mahasiswa::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
 }
-?>
